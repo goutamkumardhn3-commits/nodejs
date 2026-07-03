8f68ea714ebff2f62e6fc73ca5d273aa202cc882
goutamkumardhn3 <goutamkumardhn3@gmail.com>
2026-07-04 02:16:50 +0530

    added 4

---
+++ app.js
@@ -0,0 +1,7 @@
+module.exports = {
+    x: 101,
+    y: 201,
+    z: function() {
+        return this.x + this.y;
+    }
+}; 
---
+++ index.js
@@ -0,0 +1,48 @@
+// console.log(10);
+// let a = 5;
+// let b = 10;
+// let c = a + b;
+// console.log(c);
+
+// if(a == '5') {
+//     console.log("a is equal to 5");
+// }
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// const app = require('./app');
+// console.log(app);
+// console.log(app.x);
+// console.log(app.y);
+// console.log(app.z());
+
+// const arr = [1, 2, 3, 4, 5];
+// const result = arr.filter((num) => {
+//     return num > 2;
+// });
+// console.log(result);
+
+// const fs =  require('fs');
+// fs.writeFileSync('hello.txt', 'Hello World');
+    // console.log('==>>', __dirname);
+    // console.log('==>>', __filename);
+
+    // const fs = require('fs').writeFileSync("code.txt", "Hello World");
+    // const fs = require('fs').writeFileSync;
+    // fs("code1.txt", "Hello World");
+
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.write('<h1>Hello World</h1>');
+//     res.end();
+// }).listen(1500);
+
+const http = require('http');
+const responseHandler = (req, res) => {
+    res.write('<h1>Hello World</h1>');
+    res.end();
+    // return <div>hello Goutam</div>;
+};
+http.createServer(responseHandler).listen(1500);
